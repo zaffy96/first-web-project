ca615154a1318fcd3dfb1998908e68654cb87d9b
zaffy96 <chiarasecondoprofilo@gmail.com>
2026-05-10 17:34:16 +0200

    Update config.js

--- config.js
+++ config.js
@@ -16,13 +16,13 @@ window.countdownConfig = [
   {
     id: "concorso-letterario",
     title: "Concorso letterario",
-    targetDate: "2026-12-31T23:59:59",
+    targetDate: "2026-05-11T23:59:59",
     finishedMessage: "Il conto alla rovescia per il concorso letterario è terminato."
   },
   {
     id: "vacanze",
     title: "Vacanze",
-    targetDate: "2026-08-01T00:00:00",
+    targetDate: "2026-04-01T00:00:00",
     finishedMessage: "Le vacanze sono iniziate."
   }
 ];
